49e6ec22fcef75f6d186cce83e6fc3359f9711cc
jinhero <xiaojinhao1997@gmail.com>
2022-10-14 23:10:47 +0800

    refactor: 调整目录结构

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const logger = require('koa-logger')
 const index = require('./routes/index')
 const users = require('./routes/users')
 
+// 在页面显示的错误信息
 // error handler
 onerror(app)
 
@@ -24,18 +25,20 @@ app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }))
 
+// 和 app.use(logger()) 功能类似，这里是演示中间件的使用
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
 
+// 打印错误信息
 // error-handling
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
