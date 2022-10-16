b0863c23dd75fb814611bd6a5c759f5d7307b119
jinhero <xiaojinhao1997@gmail.com>
2022-10-16 22:56:02 +0800

    feat: 增加 session 配置

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+
+const { REDIS_CONF } = require('./conf/db')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -23,6 +27,22 @@ app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
+}))
+
+// session 配置
+app.keys = ['HJKsf_7978$#']
+app.use(session({
+  key: 'weibo.sid', // cookie name 默认是 `koa.sid`
+  prefix: 'weibo:sess:', // redis key 的前缀，默认是 `koa:sess:`
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000 // ms
+  },
+  // ttl: 24 * 60 * 60 * 1000, // 不写是默认和 cookie.maxAge 一致的
+  store: redisStore({
+    all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+  })
 }))
 
 // 和 app.use(logger()) 功能类似，这里是演示中间件的使用
