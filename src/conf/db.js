ae5d95bf127056e641cd728da28c3ebbc4f1b7bd
jinhero <xiaojinhao1997@gmail.com>
2022-10-17 21:49:26 +0800

    feat: 增加 eslint

--- src/conf/db.js
+++ src/conf/db.js
@@ -10,14 +10,32 @@ let REDIS_CONF = {
   host: '127.0.0.1',
 }
 
+let MYSQL_CONF = {
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  port: '3306',
+  database: 'koa2-weibo-db'
+}
+
 if (isProd) {
   // 线上的 redis 配置
   REDIS_CONF = {
     port: 6379,
     host: '127.0.0.1',
   }
+
+  MYSQL_CONF = {
+    // 线上的 mysql 配置
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    port: '3306',
+    database: 'koa2-weibo-db'
+  }
 }
 
 module.exports = {
-  REDIS_CONF
+  REDIS_CONF,
+  MYSQL_CONF
 }
