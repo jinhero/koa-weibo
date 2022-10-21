e78d8167b8608530e631ff6cc6c37d9078be06dc
jinhero <xiaojinhao1997@gmail.com>
2022-10-21 23:22:08 +0800

    feat: 抽离登录验证的中间件

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('koa-router')()
 
-router.get('/', async (ctx, next) => {
+const { loginRedirect, loginCheck } = require('../middlewares/loginChecks')
+
+router.get('/', loginRedirect, async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!',
     msg: '你好',
@@ -22,7 +24,7 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/json', async (ctx, next) => {
+router.get('/json', loginCheck, async (ctx, next) => {
   // const session = ctx.session
   // if (session.viewNum == null) {
   //   session.viewNum = 0
