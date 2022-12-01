7446b9bf7db55afedd8d55c2d07d8bec20f57de9
jinhero <xiaojinhao1997@gmail.com>
2022-12-01 23:08:15 +0800

    feat: 个人主页 用户数据和加载更多

--- src/conf/constant.js
+++ src/conf/constant.js
@@ -4,5 +4,9 @@
  */
 
 module.exports = {
-  DEFAULT_PICTURE: 'https://dwz.cn/rnTnftZs'
+  DEFAULT_PICTURE: '/avatar.jpeg',
+  PAGE_SIZE: 5,
+
+  // 正则表达式，匹配 '@昵称 - userName'
+  REG_FOR_AT_WHO: /@(.+?)\s-\s(\w+?)\b/g
 }
